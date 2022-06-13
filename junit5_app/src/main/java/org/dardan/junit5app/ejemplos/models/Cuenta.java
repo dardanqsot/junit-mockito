233bb5bc1522146d23d041033a236b15b144db8d
darwinQSot <quispesotodaniel@gmail.com>
2022-06-12 22:05:09 -0500

    Sesion 2: JUnit5 - Probando y afirmando excepciones con assertThrows en JUnit

--- junit5_app/src/main/java/org/dardan/junit5app/ejemplos/models/Cuenta.java
+++ junit5_app/src/main/java/org/dardan/junit5app/ejemplos/models/Cuenta.java
@@ -1,5 +1,7 @@
 package org.dardan.junit5app.ejemplos.models;
 
+import org.dardan.junit5app.ejemplos.exceptions.DineroInsuficienteException;
+
 import java.math.BigDecimal;
 import java.util.Locale;
 
@@ -29,7 +31,11 @@ public class Cuenta {
     }
 
     public void debito(BigDecimal monto) {
-        this.saldo = this.saldo.subtract(monto);
+        BigDecimal nuevoSaldo = this.saldo.subtract(monto);
+        if(nuevoSaldo.compareTo(BigDecimal.ZERO) < 0){
+            throw new DineroInsuficienteException("Dinero Insuficiente");
+        }
+        this.saldo = nuevoSaldo;
     }
 
     public void credito(BigDecimal monto) {
--- junit5_app/src/test/java/org/dardan/junit5app/ejemplos/models/CuentaTest.java
+++ junit5_app/src/test/java/org/dardan/junit5app/ejemplos/models/CuentaTest.java
@@ -1,5 +1,6 @@
 package org.dardan.junit5app.ejemplos.models;
 
+import org.dardan.junit5app.ejemplos.exceptions.DineroInsuficienteException;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
@@ -53,5 +54,17 @@ class CuentaTest {
         assertNotNull(cuenta.getSaldo());
         assertEquals(1100, cuenta.getSaldo().intValue());
         assertEquals("1100.12345", cuenta.getSaldo().toPlainString());
+    }
+
+    @Test
+    void testDineroInsuficienteExceptionCuenta() {
+        Cuenta cuenta = new Cuenta("Andres", new BigDecimal("1000.12345"));
+        Exception exception = assertThrows(DineroInsuficienteException.class, ()-> {
+           cuenta.debito(new BigDecimal(1500));
+        });
+
+        String actual =  exception.getMessage();
+        String esperado = "Dinero Insuficiente";
+        assertEquals(esperado, actual);
     }
 }
