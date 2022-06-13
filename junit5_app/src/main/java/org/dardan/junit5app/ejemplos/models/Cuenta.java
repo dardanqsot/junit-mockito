21d32bb46ab54bc34644143651003b49189d6ac5
darwinQSot <quispesotodaniel@gmail.com>
2022-06-12 21:25:02 -0500

    Sesion 2: JUnit5 - Escribiendo y ejecutando primeras pruebas unitarias con Assertions.

--- junit5_app/src/main/java/org/dardan/junit5app/ejemplos/models/Cuenta.java
+++ junit5_app/src/main/java/org/dardan/junit5app/ejemplos/models/Cuenta.java
@@ -1,10 +1,16 @@
 package org.dardan.junit5app.ejemplos.models;
 
 import java.math.BigDecimal;
+import java.util.Locale;
 
 public class Cuenta {
     private String persona;
     private BigDecimal saldo;
+
+    public Cuenta(String persona, BigDecimal saldo) {
+        this.saldo = saldo;
+        this.persona = persona;
+    }
 
     public String getPersona() {
         return persona;
---
+++ junit5_app/src/test/java/org/dardan/junit5app/ejemplos/models/CuentaTest.java
@@ -0,0 +1,20 @@
+package org.dardan.junit5app.ejemplos.models;
+
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CuentaTest {
+
+    @Test
+    void testNombreCuenta() {
+        Cuenta cuenta = new Cuenta("Andres", new BigDecimal("1000.12345"));
+        //cuenta.setPersona("Andres");
+        String esperado = "Andres";
+        String real = cuenta.getPersona();
+        assertEquals(esperado, real);
+        assertTrue(real.equals("Andres"));
+    }
+}
