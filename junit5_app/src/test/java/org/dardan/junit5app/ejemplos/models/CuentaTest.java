e0cb570b886d2fb913e9781485c874e2cdce0a5f
darwinQSot <quispesotodaniel@gmail.com>
2022-06-17 22:52:08 -0500

    Sesion 2: JUnit5 - Repitiendo pruebas con @RepeatedTest

--- junit5_app/src/test/java/org/dardan/junit5app/ejemplos/models/CuentaTest.java
+++ junit5_app/src/test/java/org/dardan/junit5app/ejemplos/models/CuentaTest.java
@@ -266,4 +266,17 @@ class CuentaTest {
         assertFalse(cuenta.getSaldo().compareTo(BigDecimal.ZERO) < 0);
         assertTrue(cuenta.getSaldo().compareTo(BigDecimal.ZERO) > 0);
     }
+
+    @DisplayName("Probando Debito Cuenta Repetir!")
+    @RepeatedTest(value=5, name = "{displayName} - Repetición numero {currentRepetition} de {totalRepetitions}")
+    void testDebitoCuentaRepetir(RepetitionInfo info) {
+        if(info.getCurrentRepetition() == 3){
+            System.out.println("estamos en la repeticion " + info.getCurrentRepetition());
+        }
+        cuenta.debito(new BigDecimal(100));
+        assertNotNull(cuenta.getSaldo());
+        assertEquals(900, cuenta.getSaldo().intValue());
+        assertEquals("900.12345", cuenta.getSaldo().toPlainString());
+    }
+
 }
