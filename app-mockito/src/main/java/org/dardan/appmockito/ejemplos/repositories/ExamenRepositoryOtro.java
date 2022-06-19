59961c158cd1647a1ae61a7b9b9f503098324c2c
darwinQSot <quispesotodaniel@gmail.com>
2022-06-19 05:16:25 -0500

    Sesion 3: Mockito - Realizando primeras pruebas con mockito

---
+++ app-mockito/src/main/java/org/dardan/appmockito/ejemplos/repositories/ExamenRepositoryOtro.java
@@ -0,0 +1,20 @@
+package org.dardan.appmockito.ejemplos.repositories;
+
+import org.dardan.appmockito.ejemplos.models.Examen;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class ExamenRepositoryOtro  implements ExamenRepository{
+
+    @Override
+    public List<Examen> findAll() {
+        try {
+            System.out.println("ExamenRepositoryOtro");
+            TimeUnit.SECONDS.sleep(5);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
--- app-mockito/src/test/java/org/dardan/appmockito/ejemplos/services/ExamenServiceImplTest.java
+++ app-mockito/src/test/java/org/dardan/appmockito/ejemplos/services/ExamenServiceImplTest.java
@@ -1,12 +1,36 @@
 package org.dardan.appmockito.ejemplos.services;
 
+import org.dardan.appmockito.ejemplos.models.Examen;
+import org.dardan.appmockito.ejemplos.repositories.ExamenRepository;
+import org.dardan.appmockito.ejemplos.repositories.ExamenRepositoryOtro;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 class ExamenServiceImplTest {
 
     @Test
     void findExamenPorNombre() {
+
+        ExamenRepository repository = mock(ExamenRepository.class);
+        ExamenService service =  new ExamenServiceImpl(repository);
+        List<Examen> datos = Arrays.asList(new Examen(5L, "Matematicas"), new Examen(6L, "Lenguaje"),
+                new Examen(7L, "Historia"));
+
+        when(repository.findAll()).thenReturn(datos);
+
+        Optional<Examen> examen = service.findExamenPorNombre("Matematicas");
+
+        assertTrue(examen.isPresent());
+        assertEquals(5L, examen.orElseThrow().getId());
+        assertEquals("Matematicas", examen.get().getNombre());
     }
 }
