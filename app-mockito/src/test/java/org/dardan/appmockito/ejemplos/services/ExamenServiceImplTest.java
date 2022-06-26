2b00a074311506db8f9d2a43fa1e2dead2013d62
darwinQSot <quispesotodaniel@gmail.com>
2022-06-25 23:26:58 -0500

    Sesion 3: Mockito -  Usando doThrow para comprobar excepciones en métodos void.

--- app-mockito/src/test/java/org/dardan/appmockito/ejemplos/services/ExamenServiceImplTest.java
+++ app-mockito/src/test/java/org/dardan/appmockito/ejemplos/services/ExamenServiceImplTest.java
@@ -206,5 +206,15 @@ class ExamenServiceImplTest {
         assertEquals(5L, captor.getValue());
     }
 
+    @Test
+    void testDoThrow() {
+        Examen examen = Datos.EXAMEN;
+        examen.setPreguntas(Datos.PREGUNTAS);
+        doThrow(IllegalArgumentException.class).when(preguntaRepository).guardarVarias(anyList());
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            service.guardar(examen);
+        });
+    }
 
 }
