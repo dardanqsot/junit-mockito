e8756eca8677eb50d8912cb12a7fde4a6ff2f300
darwinQSot <quispesotodaniel@gmail.com>
2022-06-30 23:38:13 -0500

    Sesion 4: Spring Boot - Creando las clases del modelo

---
+++ springboot_test/src/main/java/com/dardan/test/springboot/app/exceptions/DineroInsuficienteException.java
@@ -0,0 +1,7 @@
+package com.dardan.test.springboot.app.exceptions;
+
+public class DineroInsuficienteException extends RuntimeException{
+    public DineroInsuficienteException(String message) {
+        super(message);
+    }
+}
