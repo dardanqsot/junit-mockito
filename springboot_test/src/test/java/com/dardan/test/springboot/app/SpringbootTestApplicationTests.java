3b931d312de66e0717db4456ce8b2010d0da8262
darwinQSot <quispesotodaniel@gmail.com>
2022-07-01 23:59:35 -0500

    Sesion 4: Spring Boot -Escribiendo test con JUnit y mockito

--- springboot_test/src/test/java/com/dardan/test/springboot/app/SpringbootTestApplicationTests.java
+++ springboot_test/src/test/java/com/dardan/test/springboot/app/SpringbootTestApplicationTests.java
@@ -1,13 +1,63 @@
 package com.dardan.test.springboot.app;
 
+import com.dardan.test.springboot.app.repositories.BancoRepository;
+import com.dardan.test.springboot.app.repositories.CuentaRepository;
+import com.dardan.test.springboot.app.services.CuentaService;
+import com.dardan.test.springboot.app.services.CuentaServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.math.BigDecimal;
+
+import static com.dardan.test.springboot.app.Datos.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 class SpringbootTestApplicationTests {
 
+    CuentaRepository cuentaRepository;
+
+    BancoRepository bancoRepository;
+
+    CuentaService service;
+
+    @BeforeEach
+    void setUp() {
+		cuentaRepository = mock(CuentaRepository.class);
+		bancoRepository = mock(BancoRepository.class);
+		service = new CuentaServiceImpl(cuentaRepository, bancoRepository);
+//		Datos.CUENTA_001.setSaldo(new BigDecimal("1000"));
+//		Datos.CUENTA_002.setSaldo(new BigDecimal("2000"));
+//		Datos.BANCO.setTotalTransferencias(0);
+    }
+
     @Test
     void contextLoads() {
+        when(cuentaRepository.findById(1L)).thenReturn(crearCuenta001());
+        when(cuentaRepository.findById(2L)).thenReturn(crearCuenta002());
+        when(bancoRepository.findById(1L)).thenReturn(crearBanco());
+
+        BigDecimal saldoOrigen = service.revisarSaldo(1L);
+        BigDecimal saldoDestino = service.revisarSaldo(2L);
+        assertEquals("1000", saldoOrigen.toPlainString());
+        assertEquals("2000", saldoDestino.toPlainString());
+
+        service.transferir(1L, 2L, new BigDecimal("100"), 1L);
+
+        saldoOrigen = service.revisarSaldo(1L);
+        saldoDestino = service.revisarSaldo(2L);
+
+        assertEquals("900", saldoOrigen.toPlainString());
+        assertEquals("2100", saldoDestino.toPlainString());
+
+        int total = service.revisarTotalTransferencias(1L);
+        assertEquals(1, total);
+
     }
 
 }
