4781bf7a300093ab2ee581ee2d711372e4dae2ae
darwinQSot <quispesotodaniel@gmail.com>
2022-07-03 17:50:51 -0500

    Sesion 5: Spring Boot : Test de Repositorios (DataJpaTest)- modificando repositorios con Spring Data JPA

--- springboot_test/src/main/java/com/dardan/test/springboot/app/repositories/CuentaRepository.java
+++ springboot_test/src/main/java/com/dardan/test/springboot/app/repositories/CuentaRepository.java
@@ -1,13 +1,16 @@
 package com.dardan.test.springboot.app.repositories;
 
 import com.dardan.test.springboot.app.models.Cuenta;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
-public interface CuentaRepository {
-    List<Cuenta> findAll();
-
-    Cuenta findById(Long id);
-
-    void update(Cuenta cuenta);
+public interface CuentaRepository extends JpaRepository<Cuenta, Long> {
+//    List<Cuenta> findAll();
+//
+//    Cuenta findById(Long id);
+//
+//    void update(Cuenta cuenta);
 }
