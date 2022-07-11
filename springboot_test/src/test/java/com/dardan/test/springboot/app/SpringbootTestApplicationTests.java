00de6b3b4072e85d85e5f322ef2bdeb2305b7ff8
darwinQSot <quispesotodaniel@gmail.com>
2022-07-10 23:01:17 -0500

    Sesion 6: Spring Boot Test de controladores : Escribiendo pruebas unitarias para el service - findall

--- springboot_test/src/test/java/com/dardan/test/springboot/app/SpringbootTestApplicationTests.java
+++ springboot_test/src/test/java/com/dardan/test/springboot/app/SpringbootTestApplicationTests.java
@@ -14,6 +14,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
 
 import static com.dardan.test.springboot.app.Datos.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -124,4 +126,22 @@ class SpringbootTestApplicationTests {
 
         verify(cuentaRepository, times(2)).findById(1L);
     }
+
+    @Test
+    void testFindAll() {
+        // Given
+        List<Cuenta> datos = Arrays.asList(crearCuenta001().orElseThrow(), crearCuenta002().orElseThrow());
+        when(cuentaRepository.findAll()).thenReturn(datos);
+
+        // when
+        List<Cuenta> cuentas = service.findAll();
+
+        // then
+        assertFalse(cuentas.isEmpty());
+        assertEquals(2, cuentas.size());
+        assertTrue(cuentas.contains(crearCuenta002().orElseThrow()));
+
+        verify(cuentaRepository).findAll();
+    }
+
 }
